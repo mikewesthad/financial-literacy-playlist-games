02d928991db87684dbd7fe9e8319193968455c10
Michael Hadley <michaelwesthadley@gmail.com>
2018-02-01 17:06:15 -0600

    Restructure results pages to use new FadeInSequence

--- budget-guessing-game/src/pages/coffee-results.js
+++ budget-guessing-game/src/pages/coffee-results.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { formatAsIntCurrency } from "../utils";
-import FadeInSequence from "../animations/fade-in-sequence";
+import { formatAsIntCurrency } from "../utils/currency-conversion";
 import AnimateCurrency from "../animations/animate-currency";
+import { FadeInSequence, SequenceElement } from "../animations/fade-in-sequence";
 
 const actualCost = 1186;
 
@@ -16,60 +16,86 @@ export default class CoffeeGuess extends Component {
 
     const difference = Math.abs(actualCost - guess);
     let comparisonStatement = "";
-    if (actualCost > guess) {
-      comparisonStatement = (
-        <div>
-          That's <span className="green">{formatAsIntCurrency(difference)}</span> more than you
-          thought!
-        </div>
-      );
-    } else if (actualCost < guess) {
-      comparisonStatement = (
-        <div>
-          That's <span className="green">{formatAsIntCurrency(difference)}</span> less than you
-          thought!
-        </div>
-      );
+    if (difference === 0) {
+      comparisonStatement = <div>Bullseye! That's exactly what you thought.</div>;
+    } else if (difference < 100) {
+      if (actualCost > guess) {
+        comparisonStatement = (
+          <div>
+            Nice job! That's only <span className="green">{formatAsIntCurrency(difference)}</span>{" "}
+            more than you thought.
+          </div>
+        );
+      } else if (actualCost < guess) {
+        comparisonStatement = (
+          <div>
+            Nice job! That's only <span className="green">{formatAsIntCurrency(difference)}</span>{" "}
+            less than you thought.
+          </div>
+        );
+      }
     } else {
-      comparisonStatement = <div>That's exactly what you thought. Nice job!</div>;
+      if (actualCost > guess) {
+        comparisonStatement = (
+          <div>
+            That's <span className="green">{formatAsIntCurrency(difference)}</span> more than you
+            thought!
+          </div>
+        );
+      } else if (actualCost < guess) {
+        comparisonStatement = (
+          <div>
+            That's <span className="green">{formatAsIntCurrency(difference)}</span> less than you
+            thought!
+          </div>
+        );
+      }
     }
 
     return (
       <div className="fullscreen-container">
-        <div className="content results">
-          <FadeInSequence defaultDuration={0.75}>
-            <div className="section">
-              <div className="results__body-text">You guessed:</div>
-              <AnimateCurrency
-                className="results__currency-text"
-                endValue={guess}
-                duration={1}
-                delay={0.25}
-              />
-            </div>
-
-            <div className="section" delay={1}>
-              <div className="results__body-text">
-                According to a national study by Visa, the average cup of coffee costs $3.25. If you
-                bought one a day for a whole year:
+        <div className="content results text-center">
+          <FadeInSequence defaultDuration={1} defaultDelay={1}>
+            <SequenceElement delay={0}>
+              <div className="section">
+                <div className="results__body-text">You guessed:</div>
+                <AnimateCurrency
+                  className="results__currency-text"
+                  endValue={guess}
+                  duration={1}
+                  delay={0.25}
+                />
               </div>
-              <AnimateCurrency
-                className="results__currency-text"
-                endValue={actualCost}
-                duration={1}
-                delay={2}
-              />
-            </div>
+            </SequenceElement>
 
-            <div className="section" delay={1}>
-              <div className="results__body-text">{comparisonStatement}</div>
-            </div>
+            <SequenceElement>
+              <div className="section">
+                <div className="results__body-text">
+                  The average cup of coffee costs $3.25. If you bought one a day for a whole year,
+                  it would cost:
+                </div>
+                <AnimateCurrency
+                  className="results__currency-text"
+                  endValue={actualCost}
+                  duration={1}
+                  delay={2}
+                />
+              </div>
+            </SequenceElement>
 
-            <div className="section submit" delay={1}>
-              <Link to={nextRoute} className="button-link">
-                NEXT →
-              </Link>
-            </div>
+            <SequenceElement>
+              <div className="section">
+                <div className="results__body-text">{comparisonStatement}</div>
+              </div>
+            </SequenceElement>
+
+            <SequenceElement>
+              <div className="section submit">
+                <Link to={nextRoute} className="button-link">
+                  NEXT →
+                </Link>
+              </div>
+            </SequenceElement>
           </FadeInSequence>
         </div>
       </div>
