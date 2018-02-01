68fe30128aeceeed63560a19c4280fa197f8d10e
Michael Hadley <michaelwesthadley@gmail.com>
2018-02-01 17:05:41 -0600

    Add animated outro

--- budget-guessing-game/src/pages/outro.js
+++ budget-guessing-game/src/pages/outro.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FadeInSequence, SequenceElement } from "../animations/fade-in-sequence";
 
-export default function Intro({ nextRoute }) {
+import celebrate from "../images/celebrate.gif";
+
+export default function Outro() {
   return (
     <div className="fullscreen-container">
       <div className="content results">
-        <div className="section">
-          <p>Now that you can see why budgeting matters, let’s try it!</p>
-          <p>
-            Download our budgeting spreadsheet for young adults who are working only OR if you are
-            going to college visit CollegeBudgetBuilder.org.
-          </p>
-        </div>
+        <FadeInSequence defaultDuration={1} defaultDelay={0.5}>
+          <div className="section">
+            <SequenceElement delay={0}>
+              <img className="centered-image" src={celebrate} alt="" />
+            </SequenceElement>
+            <SequenceElement delay={0}>
+              <p>Nice estimating!</p>
+            </SequenceElement>
+          </div>
+          <div className="section">
+            <SequenceElement delay={0}>
+              <p>Now that you can see why budgeting matters, let’s head back to LRNG and try it.</p>
+            </SequenceElement>
+          </div>
+        </FadeInSequence>
       </div>
     </div>
   );
