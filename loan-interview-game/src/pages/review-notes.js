666be1e24c11abec61e22368183d0da2fc96a826
Michael Hadley <michaelwesthadley@gmail.com>
2018-03-28 16:20:06 -0500

    Redesign notebook page to display notes one at a time

--- loan-interview-game/src/pages/review-notes.js
+++ loan-interview-game/src/pages/review-notes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AvatarPortrait from "../components/avatar-portrait";
 import Hannah from "../images/avatars/hannah.svg";
@@ -7,54 +7,106 @@ import Taylor from "../images/avatars/taylor.svg";
 
 const Notes = ({ Svg, name, cluesFound, totalClues }) => {
   const numCluesFound = cluesFound.length;
-  let cluesDisplay;
-  if (numCluesFound === 0) {
-    cluesDisplay = <p>No clues uncovered yet. Interview {name} to learn more!</p>;
-  } else if (numCluesFound !== totalClues) {
-    cluesDisplay = (
-      <ul>
-        {cluesFound.map(clue => <li>{clue}</li>)}
-        <li>{totalClues - numCluesFound} clues left to uncover!</li>
-      </ul>
-    );
+
+  let message = "";
+  if (numCluesFound === totalClues) {
+    message = "Nice job - all clues uncovered!";
+  } else if (numCluesFound === 0) {
+    message = `No clues uncovered yet. Go interview ${name}!`;
   } else {
-    cluesDisplay = <ul>{cluesFound.map(clue => <li>{clue}</li>)}</ul>;
+    message = `${totalClues - numCluesFound} clues left to uncover!`;
   }
 
   return (
-    <div className="col-3">
-      <AvatarPortrait Svg={Svg} name={name} />
-      {cluesDisplay}
+    <div className="notebook">
+      <Svg className="notebook__portrait" />
+      <div className="text-center">
+        <div className="notebook__name text-center">{name}</div>
+      </div>
+      <ul className="notebook__clues-list">
+        {cluesFound.map(clue => <li className="notebook__clues-item">{clue}</li>)}
+      </ul>
+      <div className="notebook__clues-message text-center">{message}</div>
     </div>
   );
 };
 
-export default function ReviewNotes({ hannahTree, anthonyTree, taylorTree }) {
-  return (
-    <div className="fullscreen-container">
-      <div className="content">
-        <h1>Your Notes</h1>
-        <div className="row">
-          <Notes
-            Svg={Hannah}
-            name="Hannah"
-            cluesFound={hannahTree.cluesFound}
-            totalClues={hannahTree.totalNumClues}
-          />
-          <Notes
-            Svg={Anthony}
-            name="Anthony"
-            cluesFound={anthonyTree.cluesFound}
-            totalClues={anthonyTree.totalNumClues}
-          />
-          <Notes
-            Svg={Taylor}
-            name="Taylor"
-            cluesFound={taylorTree.cluesFound}
-            totalClues={taylorTree.totalNumClues}
-          />
+const numNotes = 3;
+const lastIndex = numNotes - 1;
+
+export default class ReviewNotes extends Component {
+  state = {
+    noteIndex: 0
+  };
+
+  goToNextNote = () => {
+    this.setState(prev => ({
+      noteIndex: prev.noteIndex < lastIndex ? prev.noteIndex + 1 : 0
+    }));
+  };
+
+  goToPreviousNote = () => {
+    this.setState(prev => ({
+      noteIndex: prev.noteIndex > 0 ? prev.noteIndex - 1 : lastIndex
+    }));
+  };
+
+  render() {
+    const { hannahTree, anthonyTree, taylorTree } = this.props;
+    const { noteIndex } = this.state;
+
+    let highlightedNote;
+    if (noteIndex === 0) {
+      highlightedNote = (
+        <Notes
+          Svg={Hannah}
+          name="Hannah"
+          cluesFound={hannahTree.cluesFound}
+          totalClues={hannahTree.totalNumClues}
+        />
+      );
+    } else if (noteIndex === 1) {
+      highlightedNote = (
+        <Notes
+          Svg={Anthony}
+          name="Anthony"
+          cluesFound={anthonyTree.cluesFound}
+          totalClues={anthonyTree.totalNumClues}
+        />
+      );
+    } else {
+      highlightedNote = (
+        <Notes
+          Svg={Taylor}
+          name="Taylor"
+          cluesFound={taylorTree.cluesFound}
+          totalClues={taylorTree.totalNumClues}
+        />
+      );
+    }
+
+    return (
+      <div className="fullscreen-container">
+        <div className="content">
+          <div className="section">
+            <h1>Your Notes</h1>
+          </div>
+
+          <div className="section">{highlightedNote}</div>
+
+          <div className="section text-center">
+            <button className="button-link" onClick={this.goToPreviousNote}>
+              {"<"}
+            </button>
+            <Link to="/interview-menu" className="button-link" style={{ margin: "0 1rem" }}>
+              Back
+            </Link>
+            <button className="button-link" onClick={this.goToNextNote}>
+              {">"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
