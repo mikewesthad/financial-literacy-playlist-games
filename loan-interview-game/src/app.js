85882223dbc31ec926e4f1eb6376ad50489070fa
Michael Hadley <michaelwesthadley@gmail.com>
2018-03-28 09:34:58 -0500

    Observable App component passes game store to Interview and EnterName components

--- loan-interview-game/src/app.js
+++ loan-interview-game/src/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { MemoryRouter, HashRouter, Route, Link, Switch, Redirect } from "react-router-dom";
-
+import gameData from "./store";
+import { observer } from "mobx-react";
+import EnterName from "./pages/enter-name";
 import Interview from "./pages/interview";
-import InterviewSelection from "./pages/interview-selection";
+import InterviewMenu from "./pages/interview-menu";
 import ConversationTree from "./conversation-tree/conversation-tree";
+import ReviewNotes from "./pages/review-notes";
 import dialogueData from "./conversation-tree/interview-dialogue-game.json";
 
 const hannahStartingNode = dialogueData.find(
@@ -22,36 +25,70 @@ const anthonyTree = new ConversationTree(dialogueData, anthonyStartingNode.id);
 const dev = process.env.REACT_APP_ENV === "development";
 const Router = dev ? HashRouter : MemoryRouter;
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          {/* <Route render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
-          {/* {!dev && <Route component={ForceVisitIndex} />} */}
+const App = observer(
+  class App extends Component {
+    render() {
+      return (
+        <Router>
+          <div>
+            {/* <Route render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
+            {/* {!dev && <Route component={ForceVisitIndex} />} */}
 
-          <Switch>
-            <Route exact path="/" render={() => <InterviewSelection />} />
+            <Switch>
+              <Route exact path="/" render={() => <EnterName gameData={gameData} />} />
 
-            <Route
-              exact
-              path="/interview-hannah"
-              render={() => <Interview conversationTree={hannahTree} name="Hannah" />}
-            />
-            <Route
-              exact
-              path="/interview-anthony"
-              render={() => <Interview conversationTree={anthonyTree} name="Anthony" />}
-            />
-            <Route
-              exact
-              path="/interview-taylor"
-              render={() => <Interview conversationTree={taylorTree} name="Taylor" />}
-            />
-          </Switch>
-        </div>
-      </Router>
-    );
+              <Route
+                exact
+                path="/start"
+                render={() => {
+                  return (
+                    <InterviewMenu
+                      gameData={gameData}
+                      numBorrowersInterviewed={gameData.borrowersInterviewed.length}
+                    />
+                  );
+                }}
+              />
+
+              <Route
+                exact
+                path="/interview-hannah"
+                render={() => (
+                  <Interview gameData={gameData} conversationTree={hannahTree} name="Hannah" />
+                )}
+              />
+              <Route
+                exact
+                path="/interview-anthony"
+                render={() => (
+                  <Interview gameData={gameData} conversationTree={anthonyTree} name="Anthony" />
+                )}
+              />
+              <Route
+                exact
+                path="/interview-taylor"
+                render={() => (
+                  <Interview gameData={gameData} conversationTree={taylorTree} name="Taylor" />
+                )}
+              />
+
+              <Route
+                exact
+                path="/review-notes"
+                render={() => (
+                  <ReviewNotes
+                    hannahTree={hannahTree}
+                    anthonyTree={anthonyTree}
+                    taylorTree={taylorTree}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
   }
-}
+);
+
 export default App;
