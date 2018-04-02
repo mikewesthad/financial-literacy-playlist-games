56ba86f4cc4ca0aecab06e1d269440e631d18a4c
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-02 15:00:37 -0500

    Initial template for credit power game

---
+++ credit-decision-game/README.md
@@ -0,0 +1,20 @@
+# "Credit Power" Game
+
+A game to teach the basic ideas of managing credit for 2018 students.
+
+Built with React & GSAP.
+
+## Running
+
+```
+npm i
+npm run start
+```
+
+## Note for Windows
+
+If you are attempting to run the deploy task, make sure to run the following in order to allow git to bump up the file character limit to 4096:
+
+```
+git config --system core.longpaths true
+```
---
+++ credit-decision-game/src/app.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { MemoryRouter, HashRouter, Route, Link, Switch, Redirect } from "react-router-dom";
+
+const dev = process.env.REACT_APP_ENV === "development";
+const Router = dev ? HashRouter : MemoryRouter;
+
+const App = class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          {/* <Route render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
+          {/* {!dev && <Route component={ForceVisitIndex} />} */}
+
+          <Route
+            render={({ location }) => (
+              <TransitionGroup>
+                <CSSTransition key={location.pathname} timeout={500} classNames="fade-transition">
+                  <Switch location={location}>
+                    <Route exact path="/" render={() => <p>Hi there</p>} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )}
+          />
+        </div>
+      </Router>
+    );
+  }
+};
+
+export default App;
