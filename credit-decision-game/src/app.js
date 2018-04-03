306e69813e3646af64d9eec6fa0341558ea7a95c
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-03 13:51:52 -0500

    Add score display to app

--- credit-decision-game/src/app.js
+++ credit-decision-game/src/app.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { MemoryRouter, HashRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 import Timeline from "./components/timeline";
+import CreditPowerDisplay from "./components/credit-power-display";
+import SavingsDisplay from "./components/savings-display";
 
 const dev = process.env.REACT_APP_ENV === "development";
 const Router = dev ? HashRouter : MemoryRouter;
@@ -10,11 +12,16 @@ const App = class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="container">
           {/* <Route render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
           {/* {!dev && <Route component={ForceVisitIndex} />} */}
 
           <Timeline />
+
+          <div className="score-container">
+            <CreditPowerDisplay />
+            <SavingsDisplay />
+          </div>
           <Route
             render={({ location }) => (
               <TransitionGroup>
