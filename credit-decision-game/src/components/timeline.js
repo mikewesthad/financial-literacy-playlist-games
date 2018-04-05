2c76f9faaa9be4d02d9c302328f2663d0afa45ba
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-05 07:37:11 -0500

    Manually create timeline SVG so it can be made interactive

--- credit-decision-game/src/components/timeline.js
+++ credit-decision-game/src/components/timeline.js
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
 
-import timelineSvg from "../images/timeline/timeline.svg";
+const Dot = (
+  x,
+  y,
+  { addLeftConnector = true, addRightConnector = true, isActive = false, isComplete = false } = {}
+) => (
+  <React.Fragment>
+    {addLeftConnector && (
+      <rect x={isActive ? x - 55 : x - 45} y={y - 3} width="20" height="6" fill="#37BF86" />
+    )}
+    <circle cx={x} cy={y} r={isActive ? "25" : "13  "} fill={isComplete ? "#fff" : "none"} />
+    <circle cx={x} cy={y} r={isActive ? "25" : "13"} stroke="#37BF86" strokeWidth="7" fill="none" />
+    {addRightConnector && (
+      <rect x={isActive ? x + 35 : x + 25} y={y - 3} width="20" height="6" fill="#37BF86" />
+    )}
+    {isActive && (
+      <React.Fragment>
+        <polygon points={`${x - 10},${y + 50} ${x + 10},${y + 50} ${x},${y + 40}`} fill="#37BF86" />
+        <text
+          x={x}
+          y={y + 75}
+          fontFamily="Glacial Indifference"
+          fill="#fff"
+          stroke="none"
+          fontSize="28"
+          textAnchor="middle"
+        >
+          April
+        </text>
+      </React.Fragment>
+    )}
+  </React.Fragment>
+);
 
 export default class Timeline extends Component {
   render() {
-    return <img className="timeline" src={timelineSvg} alt="Game Timeline" />;
+    const dots = [];
+    for (let i = 0; i < 12; i++) {
+      dots.push(
+        new Dot(50 + 90 * i, 40, {
+          addLeftConnector: i !== 0,
+          addRightConnector: i !== 11,
+          isActive: i === 2,
+          isComplete: i <= 2
+        })
+      );
+    }
+    return (
+      <svg className="timeline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1090 130">
+        {dots}
+      </svg>
+    );
   }
 }
