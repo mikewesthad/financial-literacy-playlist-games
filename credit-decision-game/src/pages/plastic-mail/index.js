13928e796331bad0413a47853f944ffa6983d96e
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-06 13:07:58 -0500

    Plastic mail decision

---
+++ credit-decision-game/src/pages/plastic-mail/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Prompt, PromptSection, PromptButton } from "../../components/decision";
+import { OneNewCardResult, NoNewCardResult, ThreeNewCardsResult } from "./results";
+import Page from "../../components/page";
+import cardStackImage from "../../images/credit-card-choice/card-stack.svg";
+import rewardsCardImage from "../../images/credit-card-choice/rewards-card.svg";
+import noCardImage from "../../images/credit-card-choice/no-card.svg";
+import "./style.css";
+
+export default class PlasticMail extends Component {
+  state = {
+    decision: null
+  };
+
+  chooseThreeCards = () => {
+    this.props.gameData.incrementCreditPower(-10);
+    this.setState({ decision: "three cards" });
+  };
+
+  chooseOneCard = () => {
+    this.props.gameData.incrementCreditPower(-5);
+    this.setState({ decision: "one card" });
+  };
+
+  chooseNoCard = () => {
+    this.props.gameData.incrementCreditPower(10);
+    this.setState({ decision: "no card" });
+  };
+
+  render() {
+    const { nextRoute } = this.props;
+    const { decision, paidInFull } = this.state;
+
+    let contents;
+    if (decision !== null) {
+      if (decision === "one card") contents = <OneNewCardResult nextRoute={nextRoute} />;
+      else if (decision === "no card") contents = <NoNewCardResult nextRoute={nextRoute} />;
+      else contents = <ThreeNewCardsResult nextRoute={nextRoute} />;
+    } else {
+      contents = (
+        <Prompt title="Plastic Mail">
+          <PromptSection>
+            <p>
+              Now that you’ve been using your first card, you notice that you’ve been getting a lot
+              of mail with credit card offers. What do you do?
+            </p>
+          </PromptSection>
+          <PromptSection className="text-center card-choice">
+            <div className="card-choice__option">
+              <img className="card-choice__image" src={cardStackImage} alt="Multiple Cards" />
+              <p className="card-choice__explanation">
+                Keep your current card and sign up for three new ones.
+              </p>
+              <PromptButton onClick={this.chooseThreeCards}>Take New Cards</PromptButton>
+            </div>
+            <div className="card-choice__option">
+              <img className="card-choice__image" src={rewardsCardImage} alt="Rewards Card" />
+              <p className="card-choice__explanation">
+                Cancel your current card and sign up for one new card with rewards.
+              </p>
+              <PromptButton onClick={this.chooseOneCard}>Take Rewards</PromptButton>
+            </div>
+            <div className="card-choice__option">
+              <img className="card-choice__image" src={noCardImage} alt="No Card" />
+              <p className="card-choice__explanation">Throw out the mail and ignore the offers.</p>
+              <PromptButton onClick={this.chooseNoCard}>Ignore Offers</PromptButton>
+            </div>
+          </PromptSection>
+        </Prompt>
+      );
+    }
+
+    return <Page transitionKey={decision}>{contents}</Page>;
+  }
+}
---
+++ credit-decision-game/src/pages/plastic-mail/results.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Results from "../../components/results/";
+
+export function ThreeNewCardsResult({ nextRoute, ...otherProps }) {
+  return (
+    <Results
+      title="Too Many Cards"
+      message="Each new card you open adds a credit inquiry which can hurt your credit power. You’ll also have to manage putting purchases on and paying off a total of four cards."
+      nextRoute={nextRoute}
+      {...otherProps}
+    />
+  );
+}
+
+export function OneNewCardResult({ nextRoute, ...otherProps }) {
+  return (
+    <Results
+      title="Keep Your Card..."
+      message="The rewards and perks are nice on this new card but you want to keep your first card as long as possible. It will be your longest item in your credit history."
+      nextRoute={nextRoute}
+      {...otherProps}
+    />
+  );
+}
+
+export function NoNewCardResult({ nextRoute, ...otherProps }) {
+  return (
+    <Results
+      title="Nice!"
+      message="Keeping your first credit card as long as possible will help your credit power."
+      nextRoute={nextRoute}
+      {...otherProps}
+    />
+  );
+}
