30aa8a0a1efaa910abdced3c57d6fd35c7709a5f
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-06 07:19:54 -0500

    Change enter name -> enter player name

--- loan-interview-game/src/pages/enter-name.js
+++ loan-interview-game/src/pages/enter-name.js
@@ -6,8 +6,7 @@ const EnterName = observer(({ gameData, nextRoute }) => (
   <div className="fullscreen-container">
     <div className="content">
       <div className="section text-center">
-        <h1>Before we start the game, what’s your name?</h1>
-        <p>(Don’t worry - it doesn’t have to be your real name.)</p>
+        <h1>Before we start the game, pick a name for your character.</h1>
       </div>
       <div className="section text-center">
         <input
