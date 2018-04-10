8cbbbcd81311fdf80aa0a4089b426816155365b9
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-10 14:27:57 -0500

    Update routes in app

--- credit-decision-game/src/app.js
+++ credit-decision-game/src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { MemoryRouter, HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { MemoryRouter, HashRouter, Route, Switch, Redirect, withRouter } from "react-router-dom";
 import Timeline from "./components/timeline";
 import { CreditPowerDisplay, SavingsDisplay } from "./components/score-display/";
 import ChoosingPlastic from "./pages/choosing-plastic/";
@@ -13,6 +13,7 @@ import PlasticMail from "./pages/plastic-mail";
 import MissingCard from "./pages/missing-card";
 import GettingOnline from "./pages/getting-online";
 import CreditCheck from "./pages/credit-check";
+import Intro from "./pages/intro";
 import gameData from "./store";
 import { observer } from "mobx-react";
 
@@ -37,39 +38,57 @@ const decisions = [
 @observer
 class App extends Component {
   render() {
+    const { creditPower, savings } = gameData;
     return (
       <Router>
-        <div className="container">
-          {/* < render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
-          {/* {!dev && <Route component={ForceVisitIndex} />} */}
-          <div className="hud">
-            <CreditPowerDisplay value={gameData.creditPower} />
-            <SavingsDisplay value={gameData.savings} />
-          </div>
+        {/* < render={props => <Analytics trackingId="UA-114340105-2" {...props} />} /> */}
+        {/* {!dev && <Route component={ForceVisitIndex} />} */}
+        <Route
+          render={({ location }) => (
+            <div className="container">
+              <div className="hud">
+                <CreditPowerDisplay value={creditPower} />
                 <Timeline decisionNumber={location.pathname.slice(1)} />
+                <SavingsDisplay value={savings} />
+              </div>
 
-          <div className="page-wrapper">
-            <Route
-              render={({ location }) => (
-                <TransitionGroup component={null}>
-                  <CSSTransition key={location.pathname} timeout={1200} classNames="fade-zoom-">
-                    <Switch location={location}>
-                      {decisions.map((Component, i) => (
-                        <Route
-                          key={i}
-                          exact
-                          path={`/${i}`}
-                          render={() => <Component gameData={gameData} nextRoute={`/${i + 1}`} />}
-                        />
-                      ))}
-                      <Redirect to="/0" />
-                    </Switch>
-                  </CSSTransition>
-                </TransitionGroup>
-              )}
-            />
-          </div>
-        </div>
+              <div className="page-wrapper">
+                <Route
+                  render={({ location }) => (
+                    <TransitionGroup component={null}>
+                      <CSSTransition key={location.pathname} timeout={1200} classNames="fade-zoom-">
+                        <Switch location={location}>
+                          <Route
+                            key="intro"
+                            exact
+                            path="/"
+                            render={() => <Intro gameData={gameData} nextRoute="/0" />}
+                          />
+
+                          {decisions.map((Component, i) => (
+                            <Route
+                              key={i}
+                              exact
+                              path={`/${i}`}
+                              render={() => (
+                                <Component
+                                  gameData={gameData}
+                                  nextRoute={i >= decisions.length - 1 ? "/" : `/${i + 1}`}
+                                />
+                              )}
+                            />
+                          ))}
+
+                          <Redirect to="/" />
+                        </Switch>
+                      </CSSTransition>
+                    </TransitionGroup>
+                  )}
+                />
+              </div>
+            </div>
+          )}
+        />
       </Router>
     );
   }
