23b1774d9d5c4b7776c98856757780f68c41572c
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-16 14:20:30 -0500

    Tightening up outro screen langauge

--- loan-interview-game/src/pages/decision-outcome/index.js
+++ loan-interview-game/src/pages/decision-outcome/index.js
@@ -5,24 +5,18 @@ import { FadeInSequence, SequenceElement } from "../../components/fade-in-sequen
 export default class DecisionOutcome extends Component {
   render() {
     const { gameData } = this.props;
-    const { selectedBorrower } = gameData;
+    let { selectedBorrower } = gameData;
 
-    let outcome;
+    const outcome;
     if (selectedBorrower === "hannah") outcome = hannahOutcome;
     else if (selectedBorrower === "taylor") outcome = taylorOutcome;
     else if (selectedBorrower === "anthony") outcome = anthonyOutcome;
     else throw new Error(`Unknown borrower selected by player: "${selectedBorrower}"`);
 
-    const formattedName = selectedBorrower.charAt(0).toUpperCase() + selectedBorrower.slice(1);
-
     return (
       <div className="fullscreen-container">
-        <FadeInSequence defaultDuration={1} defaultDelay={0.5}>
+        <FadeInSequence defaultDuration={1} defaultDelay={2}>
           <div className="content">
-            <SequenceElement delay={0}>
-              <div>You recommended {formattedName} to your boss.</div>
-            </SequenceElement>
-
             {outcome}
 
             <SequenceElement>
--- loan-interview-game/src/pages/decision-outcome/outcomes.js
+++ loan-interview-game/src/pages/decision-outcome/outcomes.js
@@ -3,15 +3,22 @@ import { SequenceElement } from "../../components/fade-in-sequence";
 
 const taylorOutcome = (
   <div>
-    <SequenceElement>
-      <p>While Taylor is your boss’s cousin, they turned out to be unreliable.</p>
-    </SequenceElement>
-    <SequenceElement delay={2}>
+    <div>
+      <SequenceElement delay={0}>
+        <span>You recommended Taylor. </span>
+      </SequenceElement>
+      <SequenceElement delay={0}>
+        <span>While Taylor is your boss’s cousin, Taylor turned out to be unreliable.</span>
+      </SequenceElement>
+    </div>
+    <SequenceElement delay={1}>
       <p>
         Without a clear plan to pay back the loan and without any history of paying bills on time,
-        they just blew through the money and have nothing to show for it. They never paid back the
-        loan.
+        they just blew through the money and have nothing to show for it.
       </p>
+    </SequenceElement>
+    <SequenceElement delay={2}>
+      <p>They never paid back the loan.</p>
     </SequenceElement>
     <SequenceElement delay={2}>
       <p>
@@ -24,33 +31,47 @@ const taylorOutcome = (
 
 const hannahOutcome = (
   <div>
-    <SequenceElement>
-      <p>While Hannah is a good friend of your boss, they turned out to be unreliable.</p>
-    </SequenceElement>
-    <SequenceElement delay={2}>
+    <div>
+      <SequenceElement delay={0}>
+        <span>You recommended Hannah. </span>
+      </SequenceElement>
+      <SequenceElement delay={0}>
+        <span>While Hannah is a good friend of your boss, Hannah turned out to be unreliable.</span>
+      </SequenceElement>
+    </div>
+    <SequenceElement delay={1}>
       <p>
-        They have a history of making payments on time and a plan to repay the loan, but they also
-        have maxed out credit cards and a history of losing their job. They used the loan to buy a
-        car, but ended up quitting their new job. Since they were already struggling with credit
-        card debit, they still haven’t paid back the loan.
+        They have made payments on time in the past and have a plan to repay the loan, but they also
+        have maxed out credit cards and a history of losing their job.
       </p>
     </SequenceElement>
     <SequenceElement delay={2}>
       <p>
-        Next time, try looking for someone who doesn’t have a lot of existing debt and who has a
-        stable job.
+        They used the loan to buy a car, but then quit their job. Since they already had existing
+        debt, they still haven’t paid back the loan.
       </p>
+    </SequenceElement>
+    <SequenceElement delay={2}>
+      <p>Next time, try looking for someone who has no debt and a stable job.</p>
     </SequenceElement>
   </div>
 );
 
 const anthonyOutcome = (
   <div>
-    <SequenceElement>
+    <div>
+      <SequenceElement delay={0}>
+        <span>You recommended Anthony. </span>
+      </SequenceElement>
+      <SequenceElement delay={0}>
+        <span>Anthony was a perfect choice!</span>
+      </SequenceElement>
+    </div>
+    <SequenceElement delay={1}>
       <p>
-        Anthony was a perfect choice! They had a repayment plan, a stable job and a history of
-        paying off loans. They were able to take the night classes, get the new job in marketing at
-        the library and pay your boss back all within a year.
+        They had a repayment plan, a stable job and a history of paying off loans. They were able to
+        take the night classes, get the new job in marketing at the library and pay your boss back
+        all within a year.
       </p>
     </SequenceElement>
   </div>
