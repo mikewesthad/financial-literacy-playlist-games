257eb11ebb942f87908971fb9297bbba44591549
Michael Hadley <michaelwesthadley@gmail.com>
2018-04-20 14:58:20 -0500

    Extend checkbox to have a disabled state

--- credit-decision-game/src/pages/transaction-detective/checkbox.js
+++ credit-decision-game/src/pages/transaction-detective/checkbox.js
@@ -2,9 +2,14 @@ import React from "react";
 import checkmarkImage from "../../images/icons/checkmark.svg";
 import "./checkbox.css";
 
-export default function Checkbox({ checked, className, ...otherProps }) {
+export default function Checkbox({ checked, className, disabled, ...otherProps }) {
   return (
-    <div className={`checkbox ${checked ? "checkbox--checked" : ""} ${className}`} {...otherProps}>
+    <div
+      className={`checkbox ${checked ? "checkbox--checked" : ""} ${
+        disabled ? "checkbox--disabled" : "checkbox--interactive"
+      } ${className}`}
+      {...otherProps}
+    >
       <img
         draggable={false}
         width={checked ? "100%" : "0px"}
